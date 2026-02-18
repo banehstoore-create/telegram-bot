807e505c73050eeb637f3c11c9239c1466cf3f8e
banehstoore-create <banehstoore@gmail.com>
2026-02-18 12:33:09 +0330

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,17 @@
+from flask import Flask
+from threading import Thread
+
+# اول این خط باید باشد
+app = Flask('')
+
 @app.route('/')
 def home():
-    return "OK" # فقط یک کلمه کوتاه برگردانید
+    return "OK"
+
+def run():
+    # پورت Render معمولاً 80 یا 10000 است اما 8080 هم جواب می‌دهد
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
